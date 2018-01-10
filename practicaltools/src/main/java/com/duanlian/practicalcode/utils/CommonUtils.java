dab7c3c610eb96c1196a4ba18f3b7da8fd4c0075
duanlian <duanlian0105@126.com>
2018-01-10 11:07:48 +0800

    Initial commit

---
+++ practicaltools/src/main/java/com/duanlian/practicalcode/utils/CommonUtils.java
@@ -0,0 +1,275 @@
+package com.duanlian.practicalcode.utils;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.security.MessageDigest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 通用工具类 段炼
+ */
+public class CommonUtils {
+    private static final String GSON_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    public static SimpleDateFormat formatDay = new SimpleDateFormat("d", Locale.getDefault());
+    public static SimpleDateFormat formatMonthDay = new SimpleDateFormat("M-d", Locale.getDefault());
+    public static SimpleDateFormat formatDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+
+    private CommonUtils() {
+    }
+
+
+    /**
+     * 格式化日期
+     *
+     * @param date
+     * @return 年月日
+     */
+    public static String formatDate(Date date) {
+        return formatDate.format(date);
+    }
+
+    /**
+     * 格式化日期
+     *
+     * @param date
+     * @return 年月日 时分秒
+     */
+    public static String formatDateTime(Date date) {
+        return formatDateTime.format(date);
+    }
+
+    /**
+     * 将时间戳解析成日期
+     *
+     * @param timeInMillis
+     * @return 年月日
+     */
+    public static String parseDate(long timeInMillis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timeInMillis);
+        Date date = calendar.getTime();
+        return formatDate(date);
+    }
+
+    /**
+     * 将时间戳解析成日期
+     *
+     * @param timeInMillis
+     * @return 年月日 时分秒
+     */
+    public static String parseDateTime(long timeInMillis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timeInMillis);
+        Date date = calendar.getTime();
+        return formatDateTime(date);
+    }
+
+    /**
+     * 解析日期
+     *
+     * @param date
+     * @return
+     */
+    public static Date parseDate(String date) {
+        Date mDate = null;
+        try {
+            mDate = formatDate.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return mDate;
+    }
+
+    /**
+     * 解析日期
+     *
+     * @param datetime
+     * @return
+     */
+    public static Date parseDateTime(String datetime) {
+        Date mDate = null;
+        try {
+            mDate = formatDateTime.parse(datetime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return mDate;
+    }
+
+    /**
+     * 对指定字符串进行md5加密
+     *
+     * @param s
+     * @return 加密后的数据
+     */
+    public static String EncryptMD5(String s) {
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f'};
+        try {
+            byte[] btInput = s.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 判断email格式是否正确
+     *
+     * @param email
+     * @return
+     */
+    public static boolean isEmail(String email) {
+        String str = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+        Pattern p = Pattern.compile(str);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+
+    /**
+     * Try to return the absolute file path from the given Uri
+     *
+     * @param context
+     * @param uri
+     * @return the file path or null
+     */
+    public static String getRealFilePath(final Context context, final Uri uri) {
+        if (null == uri) return null;
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null)
+            data = uri.getPath();
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
+
+
+    /**
+     * 调用震动器
+     *
+     * @param context      调用该方法的Context
+     * @param milliseconds 震动的时长，单位是毫秒
+     */
+    public static void vibrate(final Context context, long milliseconds) {
+        Vibrator vib = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
+        vib.vibrate(milliseconds);
+    }
+
+    /**
+     * 调用震动器
+     *
+     * @param context  调用该方法的Context
+     * @param pattern  自定义震动模式 。数组中数字的含义依次是[静止时长，震动时长，静止时长，震动时长。。。]时长的单位是毫秒
+     * @param isRepeat 是否反复震动，如果是true，反复震动，如果是false，只震动一次
+     */
+    public static void vibrate(final Context context, long[] pattern, boolean isRepeat) {
+        Vibrator vib = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
+        vib.vibrate(pattern, isRepeat ? 1 : -1);
+    }
+
+    /**
+     * 获取联系人电话
+     *
+     * @param cursor
+     * @return
+     */
+    public String getContactPhone(Context context, Cursor cursor) {
+
+        int phoneColumn = cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+        int phoneNum = cursor.getInt(phoneColumn);
+        String phoneResult = "";
+        //System.out.print(phoneNum);
+        if (phoneNum > 0) {
+            // 获得联系人的ID号
+            int idColumn = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+            String contactId = cursor.getString(idColumn);
+            // 获得联系人的电话号码的cursor;
+            Cursor phones = context.getContentResolver().query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId,
+                    null, null);
+            //int phoneCount = phones.getCount();
+            //allPhoneNum = new ArrayList<String>(phoneCount);
+            if (phones.moveToFirst()) {
+                // 遍历所有的电话号码
+                for (; !phones.isAfterLast(); phones.moveToNext()) {
+                    int index = phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    int typeindex = phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE);
+                    int phone_type = phones.getInt(typeindex);
+                    String phoneNumber = phones.getString(index);
+                    switch (phone_type) {
+                        case 2:
+                            phoneResult = phoneNumber;
+                            break;
+                    }
+                    //allPhoneNum.add(phoneNumber);
+                }
+                if (!phones.isClosed()) {
+                    phones.close();
+                }
+            }
+        }
+        return phoneResult;
+    }
+
+    /**
+     * 传入图片资源返回imageView格式
+     * @param resId
+     * @param context
+     * @return
+     */
+    public static ImageView getImageView(int resId, Context context) {
+        ImageView image = new ImageView(context);
+        image.setImageResource(resId);
+        image.setScaleType(ImageView.ScaleType.FIT_XY);
+        return image;
+    }
+}
---
+++ practicaltools/src/main/java/com/duanlian/practicalcode/utils/LogUtils.java
@@ -0,0 +1,64 @@
+package com.duanlian.practicalcode.utils;
+
+import android.content.Context;
+import android.util.Log;
+
+
+/**
+ * 日志管理器 段炼
+ */
+
+public class LogUtils {
+
+    /**
+     * 是否为开发者模式(开发模式打印LOG,非开发模式不打印LOG)
+     */
+    private static boolean mDebug = true;
+
+    private LogUtils() {
+    }
+
+    /**
+     * 打印info级别的log
+     *
+     * @param msg
+     */
+    public static void i(Context context, String msg) {
+        if (mDebug) {
+            Log.i("*** CurrentPage：" + context.getClass().getSimpleName() + "  Log", msg);
+        }
+    }
+
+    /**
+     * 打印info级别的log
+     *
+     * @param msg
+     */
+    public static void i(String msg) {
+        if (mDebug) {
+            Log.i("Log-----info", msg);
+        }
+    }
+
+    /**
+     * 打印error级别的log
+     *
+     * @param msg
+     */
+    public static void e(Context context, String msg) {
+        if (mDebug) {
+            Log.e("*** CurrentPage：" + context.getClass().getSimpleName() + "  Log", msg);
+        }
+    }
+
+    /**
+     * 打印error级别的log
+     *
+     * @param msg
+     */
+    public static void e(String msg) {
+        if (mDebug) {
+            Log.e("Log-----error", msg);
+        }
+    }
+}
